877d23bd19164944962590ab86a949263b3d6c93
RealThomasMiles <dmilya2000@gmail.com>
2020-12-12 12:16:59 +0300

    Added end screen

---
+++ scripts/ending.js
@@ -0,0 +1,34 @@
+var downloadForm = document.getElementById('download-form');
+var nameHere = document.getElementById('name-here');
+var resultsElem = document.getElementById('results');
+
+var playerInfo = JSON.parse(localStorage.getItem('playerInfo'));
+var nameNode = document.createTextNode(playerInfo.name);
+nameHere.appendChild(nameNode);
+var br = document.createElement('br');
+resultsElem.innerText = 'The Trapezoid Game - Number of moves: ' + playerInfo.level1;
+resultsElem.appendChild(br);
+resultsElem.innerText += 'The Shape-Cutting Game - Your time: ' + playerInfo.level2;
+resultsElem.appendChild(br);
+resultsElem.innerText += 'The Shape-Cutting Game II - Your range: ' + playerInfo.level3;
+
+const downloadToFile = (content, filename, contentType) => {
+    const a = document.createElement('a');
+    const file = new Blob([content], {type: contentType});
+
+    a.href= URL.createObjectURL(file);
+    a.download = filename;
+    a.click();
+
+    URL.revokeObjectURL(a.href);
+};
+
+downloadForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const text = `Results for ${playerInfo.name}` + '\r\n\r\n' +
+        'The Trapezoid Game - Number of moves: ' + playerInfo.level1 + '\r\n' +
+        'The Shape-Cutting Game - Your time: ' + playerInfo.level2 + 's' + '\r\n' +
+        'The Shape-Cutting Game II - Your range: ' + playerInfo.level3 + '\r\n\r\n' +
+        'ShapeS Extravaganza by Thomas Miles - http://shapesgame.thomasmiles.ml/'
+    downloadToFile(text, `shapes-game-results-${Date.now()}`, 'text/plain');
+})
